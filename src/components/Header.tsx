17dce4a6aab559d2c053f6d606c455e1e265e3cb
Enes Ertuğrul Koyuncu <enes.koyuncu5507@gmail.com>
2025-01-12 13:46:23 +0300

    Login Temp done/ not gonna change

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,45 +4,20 @@ import nskLogo from "../img/file.jpeg";
 export default function Header() {
   return (
     <div className="header-main">
-      <div className="header-left">
-        <div className="header-left-top">
-          <div className="header-left-top-left">
-            <a href="#">Anasayfa</a>
-          </div>
-          <div className="header-left-top-mid"></div>
-          <div className="header-left-top-right">
-            <a href="#">Projeler</a>
-          </div>
-        </div>
-        <div className="header-left-bottom">
-          <div className="header-left-bottom-left"></div>
-          <div className="header-left-bottom-mid">
-            <a href="#">CV</a>
-          </div>
-          <div className="header-left-bottom-right"></div>
-        </div>
+      <div className="header-links header-left">
+        <a href="#home">Anasayfa</a>
+        <a href="#cv">CV</a>
+        <a href="#projects">Projeler</a>
       </div>
-      <div className="header-mid">
+
+      <div className="header-logo">
         <img src={nskLogo} alt="nsk logo" />
       </div>
 
-      <div className="header-right">
-        <div className="header-right-top">
-          <div className="header-right-top-left"></div>
-          <div className="header-right-top-mid">
-            <a href="#">Blog</a>
-          </div>
-          <div className="header-right-top-right"></div>
-        </div>
-        <div className="header-right-bottom">
-          <div className="header-right-bottom-left">
-            <a href="#">Hakkımda</a>
-          </div>
-          <div className="header-right-bottom-mid"></div>
-          <div className="header-right-bottom-right">
-            <a href="#">İletişim</a>
-          </div>
-        </div>
+      <div className="header-links header-right">
+        <a href="#about">Hakkımda</a>
+        <a href="#blog">Blog</a>
+        <a href="#contact">İletişim</a>
       </div>
     </div>
   );
